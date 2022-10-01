442ad24985408df15ba1ccd5307fce7387357dc5
Maria Libkind <maria.libkind@mail.ru>
2022-10-01 13:30:03 +0300

    попытка

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const profileButton = document.querySelector(".profile__edit-btn");
+
+function openProfilePopup(){
+    // нужно поменять класс у попапа на эктив 
+    
+}
+
+profileButton.addEventListener("click", openProfilePopup)
