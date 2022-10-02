0f312fb12acec203e114f6226d4dc8c06982fad6
Maria Libkind <maria.libkind@mail.ru>
2022-10-02 19:58:16 +0300

    стараюсь

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,27 @@
-const profileButton = document.querySelector(".profile__edit-btn");
+const POPUP_ACTIVE_CLASS = document.querySelector(".popup_active");
 
-function openProfilePopup(){
-    // нужно поменять класс у попапа на эктив 
-    
-}
+const openProfileBtn = document.querySelector(".profile__edit-btn");
+const profile = document.querySelector(".popup");
+const profileContent = document.querySelector(".popup__content");
+const profileCloseButton = document.querySelector(".popup__cross-icon");
+const form = document.querySelector(".popup__text");
+const nameInput = document.querySelector(".popup__text_name");
+const nameInput = document.querySelector(".popup__text_about");
 
-profileButton.addEventListener("click", openProfilePopup)
+const func = () => 1;
+
+console.log(func());
+
+openProfilePopup.addEventListener("click", function(){
+    popup.classList.add(POPUP_ACTIVE_CLASS);
+});
+
+popup.addEventListener("submit", (event) => {
+    if (!popupContent.contains(event.target) || event.target === profileCloseButton) {
+        popup.classList.remove(POPUP_ACTIVE_CLASS);
+    }
+});
+
+form.addEventListener("submit", () => {
+    form.querySelector(".form__text")
+});
