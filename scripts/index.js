4e84952ed689c52189b27e15d435979801a8aedf
Maria Libkind <maria.libkind@mail.ru>
2022-10-16 22:55:11 +0300

    новый скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -37,37 +37,33 @@ form.addEventListener('submit', formSubmitHandler);
 
 const initialCards = [
     { name: 'Архыз', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg' },
-    { name: 'Челябинская область',link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg' },
+    { name: 'Челябинская область', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg' },
     { name: 'Иваново', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg' },
     { name: 'Камчатка', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg' },
     { name: 'Холмогорский район', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg' },
     { name: 'Байкал', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg' }
-] 
+]
 const openImagePopup = document.querySelector(".profile__add-btn");
 const closeImagePopup = document.querySelector(".cross");
-const elements =  document.querySelector(".elements");
-const elementName = document.querySelector(".element__name");
-const elementImage = document.querySelector(".element__image");
-const elementsTemplate =  document.querySelector("#elements-template").content;
-const popupImage =  document.querySelector(".popup-image");
-const imageForm =  document.querySelector(".popup__form_add_image");
-const inputImageName =  document.querySelector(".popup__text_type_image-name");
-const inputImageSrc =  document.querySelector(".popup__text_type_image-src");
+const elements = document.querySelector(".elements");
+const elementsTemplate = document.querySelector("#elements-template").content;
+const element = elementsTemplate.querySelector(".element").cloneNode(true);
+const elementName = element.querySelector(".element__name");
+const elementImage = element.querySelector(".element__image");
+const popupImage = document.querySelector(".popup-image");
+const imageForm = document.querySelector(".popup__form_add_image");
+const inputImageName = document.querySelector(".popup__text_type_image-name");
+const inputImageSrc = document.querySelector(".popup__text_type_image-src");
 const elementsList = document.querySelector('.elements__list');
 
 
 //нажатие сердечка
-// function addLike() {
-//     const elementTemplate = document.querySelector('#element-template').content;
-//     const element = elementTemplate.querySelector('.element').cloneNode(true);
-//     element.querySelector('.element__like').addEventListener('click', function (evt) {
-//         evt.target.classList.toggle('.element__like_active');
-//     });
-// }
-
-// let like = document.querySelector('element__like');
-// like.addEventListener( 'click', () => 
-// like.classList.toggle('element__like_active'))
+const likeButton = element.querySelector('#element-template');
+function like(event) {
+    const likeButton = event.target;
+    likeButton.classList.toggle('element__like_active');
+}
+likeButton.addEventListener('click', like);
 
 
 // открытие попапа для добавления картинки
@@ -76,19 +72,23 @@ function openImgPopup() {
 }
 openImagePopup.addEventListener('click', openImgPopup);
 
-closeImagePopup.addEventListener('click', closePopup);
+function closePopup(event, popup) {
+    inputImageName.textContent = elementName.value;
+    inputImageSrc.textContent = elementImage.value;
+    closeImagePopup.addEventListener('click', (event) => closePopup(event, popup))
+}
+
 
 
 // добавление картинки
-// function addNewElement(evt, initialPlace) {
-//     const newElement = addElement(initialPlace);
-//     placesList.prepend(newElement);
-//     if (evt) {
-//         evt.preventDefault();
-//         closeProfilePopup(evt);
-//     }
+// function addElement(link, name) {
+//   elementImage.src = link;
+//   elementName.textContent = name;
 // };
 
+// const uploadElements () {
+
+// }
 
 
 
@@ -99,9 +99,8 @@ closeImagePopup.addEventListener('click', closePopup);
 // }
 // deleteElement.addEventListener('click', deleteElement);
 
-
-
 // открыть картинку в полный экран
+// const openFullscreenPopup = document.querySelector(".popup-fullscreen");
 // function opеnImage() {
 //     popupImage.classList.add("popup__fullscreen-image_active");
 // }
