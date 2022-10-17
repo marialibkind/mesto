ed9bc7f6cc1be9a765e0b60376e7485e2e482700
Maria Libkind <maria.libkind@mail.ru>
2022-10-17 22:58:50 +0300

    новый скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const openProfilePopup = document.querySelector(".profile__edit-btn");
 const popup = document.querySelector(".popup-profile");
 const popupContent = document.querySelector(".popup__container");
 const closeProfilePopup = document.querySelector(".cross");
-const form = document.querySelector(".popup__form");
+const form = document.querySelector(".sumbitProfile");
 const nameInput = document.querySelector(".popup__text_type_name");
 const infoInput = document.querySelector(".popup__text_type_about");
 const profileName = document.querySelector(".profile__name");
@@ -18,7 +18,7 @@ openProfilePopup.addEventListener('click', openProfile);
 
 
 // закрытие попапа
-function closePopup() {
+function closePopup(popup) {
     popup.classList.remove("popup_active");
 }
 closeProfilePopup.addEventListener('click', closePopup);
@@ -31,7 +31,7 @@ function formSubmitHandler(evt) {
     profileName.textContent = nameInput.value;
     profileInfo.textContent = infoInput.value;
 
-    closeProfile()
+    closePopup(popup)
 }
 form.addEventListener('submit', formSubmitHandler);
 
@@ -44,27 +44,14 @@ const initialCards = [
     { name: 'Байкал', link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg' }
 ]
 const openImagePopup = document.querySelector(".profile__add-btn");
-const closeImagePopup = document.querySelector(".cross");
+const closeImagePopup = document.querySelector(".cross-image");
 const elements = document.querySelector(".elements");
 const elementsTemplate = document.querySelector("#elements-template").content;
-const element = elementsTemplate.querySelector(".element").cloneNode(true);
-const elementName = element.querySelector(".element__name");
-const elementImage = element.querySelector(".element__image");
 const popupImage = document.querySelector(".popup-image");
 const imageForm = document.querySelector(".popup__form_add_image");
 const inputImageName = document.querySelector(".popup__text_type_image-name");
 const inputImageSrc = document.querySelector(".popup__text_type_image-src");
 const elementsList = document.querySelector('.elements__list');
-
-
-//нажатие сердечка
-const likeButton = element.querySelector('#element-template');
-function like(event) {
-    const likeButton = event.target;
-    likeButton.classList.toggle('element__like_active');
-}
-likeButton.addEventListener('click', like);
-
 
 // открытие попапа для добавления картинки
 function openImgPopup() {
@@ -72,36 +59,72 @@ function openImgPopup() {
 }
 openImagePopup.addEventListener('click', openImgPopup);
 
-function closePopup(event, popup) {
-    inputImageName.textContent = elementName.value;
-    inputImageSrc.textContent = elementImage.value;
-    closeImagePopup.addEventListener('click', (event) => closePopup(event, popup))
+
+// закрытие попапа для добавления картинки
+function closeImgPopup() {
+    closePopup(popupImage)
 }
+closeImagePopup.addEventListener('click', closeImgPopup);
 
-
+// создание картинки
+function createCard(link, name) {
+    const element = elementsTemplate.querySelector(".element").cloneNode(true);
+    const elementImage = element.querySelector(".element__image");
+    const elementName = element.querySelector(".element__name");
+    elementImage.src = link;
+    elementName.textContent = name;
+    elementImage.alt = name;
+    element.querySelector(".element__like").addEventListener('click', function (evt) {
+        const likeButton = evt.target;
+        likeButton.classList.toggle("element__like_active");
+    })
+    //   удаление картинки
+    element.querySelector(".element__bin").addEventListener('click', function (evt) {
+        element.remove();
+    })
+    // // открыть картинку на весь экран
+    const fullPopup = document.querySelector(".popup-fullscreen");
+    function openFullPopup() {
+        fullPopup.classList.add("popup_active");
+    }
+    elementImage.addEventListener('click', openFullPopup);
+    const fullName = document.querySelector(".full-name");
+    popupImage.addEventListener('click', () => {
+        fullPopup.setAttribute('src', link);
+        fullPopup.setAttribute('alt', name);
+        elementImage.textContent = placeName;
+    });
+    return element;
+};
 
 // добавление картинки
-// function addElement(link, name) {
-//   elementImage.src = link;
-//   elementName.textContent = name;
-// };
+function renderCard(card) {
+    elementsList.prepend(card);
+}
 
-// const uploadElements () {
+initialCards.forEach((card) => {
+    renderCard(createCard(card.link, card.name));
+})
 
+//  сохранение информации
+function imageSubmitHandler(evt) {
+    evt.preventDefault();
+    const imageName = inputImageName.value;
+    const imageLink = inputImageSrc.value;
+    if (imageLink || imageLink) {
+        renderCard(createCard(imageLink, imageName))
+    }
+    closePopup(popupImage);
+
+}
+imageForm.addEventListener('submit', imageSubmitHandler);
+closeImagePopup.addEventListener('click', imageSubmitHandler);
+
+
+
+//  закрытие картинки на весь экран
+// const closeFullPopup = document.querySelector(".cross-full");
+// function closeFullPopup() {
+//     closePopup(fullPopup)
 // }
-
-
-
-
-// //  удаление картинки
-// function deleteElement() {
-//    element.classList.remove("element__bin");
-// }
-// deleteElement.addEventListener('click', deleteElement);
-
-// открыть картинку в полный экран
-// const openFullscreenPopup = document.querySelector(".popup-fullscreen");
-// function opеnImage() {
-//     popupImage.classList.add("popup__fullscreen-image_active");
-// }
-// openImage.addEventListener('click', openImage;
+// closeFullPopup.addEventListener('click', closeFullPopup);
