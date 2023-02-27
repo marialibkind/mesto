fe33a6a9dbd6ad29246214ed8b74c9904d43284b
Maria Libkind <maria.libkind@mail.ru>
2023-02-27 22:40:09 +0300

    2702

---
+++ src/index.js
@@ -0,0 +1,93 @@
+import "./index.css";
+import { Card } from "./scripts/Card.js";
+import { FormValidator } from "./scripts/FormValidator.js";
+import { validationConfig, initialCards } from "./variables/const.js";
+import { Section } from "./scripts/Section.js";
+import { PopupWithImage } from "./scripts/PopupWithImage.js";
+import { PopupWithForm } from "./scripts/PopupWithForm.js";
+import UserInfo from "./scripts/UserInfo.js";
+import {
+  formProfile,
+  imageForm,
+  openImagePopupButton,
+  infoInput,
+  nameInput,
+  openProfilePopupButton,
+} from "./variables/elements.js";
+
+//ЭКЗЕМПЛЯРЫ КЛАССОВ
+const popupImage = new PopupWithImage(".popup-fullscreen");
+
+const popupCard = new PopupWithForm(handleSubmitCard, ".popup-image");
+popupCard.setEventListeners();
+
+const popupUser = new PopupWithForm(handleProfileFormSubmit, ".popup-profile");
+popupUser.setEventListeners();
+
+const userInfo = new UserInfo({
+  name: ".profile__name",
+  info: ".profile__info",
+});
+
+const cardsSection = new Section(
+  {
+    items: initialCards,
+    renderer: (cardItem) => {
+      const element = createCard(cardItem);
+      cardsSection.addItem(element);
+    },
+  },
+  ".elements__list"
+);
+cardsSection.renderItems();
+
+//КОЛБЭК ФУНКЦИИ
+function handleSubmitCard(value) {
+  const newNewCard = createCard({
+    name: value.enterName,
+    link: value.enterInfo,
+  });
+  cardsSection.addItem(newNewCard);
+
+  popupCard.close();
+}
+
+function handleProfileFormSubmit(value) {
+  userInfo.setUserInfo(value.enterName, value.enterInfo);
+  popupUser.close();
+}
+
+function openImagePopup(name, link) {
+  popupImage.open({ link, name });
+}
+
+//ФУНКЦИИ
+function createCard(element) {
+  const newCard = new Card(element, "#element-template", openImagePopup);
+  return newCard.createCard();
+}
+
+//СЛУШАТЕЛИ
+openImagePopupButton.addEventListener("click", () => {
+
+  popupImageValidation.clearForm();
+  popupCard.open();
+
+});
+
+openProfilePopupButton.addEventListener("click", () => {
+
+  popupProfileValidation.clearForm();
+  const { name, info } = userInfo.getUserInfo();
+  nameInput.value = name;
+  infoInput.value = info;
+
+  popupUser.open();
+});
+
+//ВАЛИДАЦИИ
+const popupImageValidation = new FormValidator(validationConfig, imageForm);
+const popupProfileValidation = new FormValidator(validationConfig, formProfile);
+
+popupImageValidation.enableValidation();
+popupProfileValidation.enableValidation();
